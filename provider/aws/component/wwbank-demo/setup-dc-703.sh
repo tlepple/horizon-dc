cff3eb57cb84b9c09d337f2cfa248dec24152f58
root <rtlepple@gmail.com>
2020-04-22 13:30:31 -0500

    initial commit

--- provider/aws/component/wwbank-demo/setup-dc-703.sh
+++ provider/aws/component/wwbank-demo/setup-dc-703.sh
@@ -102,15 +102,61 @@ cd ..
 
 echo "Sleeping for 45s..."
 sleep 45
+
 ###########################################################################################################
-#
+# Create KDC users and keytabs
 ###########################################################################################################
+echo "Creating users in KDC..."
+kadmin.local -q "addprinc -randkey joe_analyst/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey kate_hr/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey log_monitor/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey diane_csr/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey jermy_contractor/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey mark_bizdev/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey john_finance/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey ivanna_eu_hr/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "addprinc -randkey etl_user/$(hostname -f)@${kdc_realm}"
+
+
+echo "Creating user keytabs..."
+mkdir -p /etc/security/keytabs
+cd /etc/security/keytabs
+kadmin.local -q "xst -k joe_analyst.keytab joe_analyst/$(hostname -f)@${kdc_realm}"    
+kadmin.local -q "xst -k log_monitor.keytab log_monitor/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k diane_csr.keytab diane_csr/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k jermy_contractor.keytab jermy_contractor/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k mark_bizdev.keytab mark_bizdev/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k john_finance.keytab john_finance/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k ivanna_eu_hr.keytab ivanna_eu_hr/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k kate_hr.keytab kate_hr/$(hostname -f)@${kdc_realm}"
+kadmin.local -q "xst -k etl_user.keytab etl_user/$(hostname -f)@${kdc_realm}" 
+chmod +r *.keytab
+
+kinit -kt /etc/security/keytabs/etl_user.keytab  etl_user/$(hostname -f)@${kdc_realm}
+hdfs dfs -mkdir -p /apps/hive/share/udfs/
+hdfs dfs -put /opt/cloudera/parcels/CDH/lib/hive/lib/hive-exec.jar /apps/hive/share/udfs/
+hdfs  dfs -chown -R hive:hadoop  /apps
+
+
+###########################################################################################################
+# 
+###########################################################################################################
+
+cd /root/horizon-dc/provider/aws/component/wwbank-demo
+
+./scripts/02-create-hdfs-user-folders.sh
+./scripts/03-copy-data-to-hdfs-dc.sh
+hdfs dfs -ls -R /hive_data
 
 ###########################################################################################################
 #
 ###########################################################################################################
-
 ###########################################################################################################
 #
 ###########################################################################################################
-
+###########################################################################################################
+#
+###########################################################################################################
+###########################################################################################################
+#
+###########################################################################################################
