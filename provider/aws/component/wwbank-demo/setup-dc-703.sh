3af2747290b298a3d4aa622ee9154c16eaed8549
root <rtlepple@gmail.com>
2020-04-22 08:25:33 -0500

    initial commit

--- provider/aws/component/wwbank-demo/setup-dc-703.sh
+++ provider/aws/component/wwbank-demo/setup-dc-703.sh
@@ -19,12 +19,80 @@ export host=$(hostname -f)
 
 
 ###########################################################################################################
-# 
+#  install nc & jq 
 ###########################################################################################################
 yum install -y nc
 
 #  install jq from util function
 install_jq_cli
+
+###########################################################################################################
+# setup OS Users & groups
+###########################################################################################################
+./scripts/01-create-os-users.sh
+
+echo "Waiting 60s for Ranger usersync..."
+sleep 60
+
+###########################################################################################################
+#  Import Ranger items
+###########################################################################################################
+iranger_curl="curl -u admin:${ranger_password}"
+ranger_url="http://localhost:6080/service"
+
+
+${ranger_curl} -X POST -H "Content-Type: application/json" -H "Accept: application/json" ${ranger_url}/public/v2/api/roles  -d @- <<EOF
+{
+   "name":"Admins",
+   "description":"",
+   "users":[
+   ],
+   "groups":[
+      {
+         "name":"etl",
+         "isAdmin":false
+      }
+   ],
+   "roles":[
+   ]
+}
+EOF
+
+
+
+${ranger_curl} ${ranger_url}/public/v2/api/servicedef/name/hive \
+  | jq '.options = {"enableDenyAndExceptionsInPolicies":"true"}' \
+  | jq '.policyConditions = [
+{
+	  "itemId": 1,
+	  "name": "resources-accessed-together",
+	  "evaluator": "org.apache.ranger.plugin.conditionevaluator.RangerHiveResourcesAccessedTogetherCondition",
+	  "evaluatorOptions": {},
+	  "label": "Resources Accessed Together?",
+	  "description": "Resources Accessed Together?"
+},{
+	"itemId": 2,
+	"name": "not-accessed-together",
+	"evaluator": "org.apache.ranger.plugin.conditionevaluator.RangerHiveResourcesNotAccessedTogetherCondition",
+	"evaluatorOptions": {},
+	"label": "Resources Not Accessed Together?",
+	"description": "Resources Not Accessed Together?"
+}
+]' > hive.json
+
+${ranger_curl} -i \
+  -X PUT -H "Accept: application/json" -H "Content-Type: application/json" \
+  -d @hive.json ${ranger_url}/public/v2/api/servicedef/name/hive
+sleep 10
+
+
+###########################################################################################################
+#
+###########################################################################################################
+
+###########################################################################################################
+#
+###########################################################################################################
 
 ###########################################################################################################
 #
